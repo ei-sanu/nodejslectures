544307a3d7ebadbb539fa92e35919a15fad46929
ei-sanu <someshranjanbiswal13678@gmail.com>
2026-01-28 16:28:21 +0530

    first commit

---
+++ demoevent.js
@@ -0,0 +1,41 @@
+var EventEmitter = require('events')        //import the module
+var obj = new EventEmitter()   //created an object of the event emitter class
+
+
+obj.addListener('marketOpen', (msg) => {
+    console.log(msg)
+})
+obj.emit('marketOpen', 'The market is now open')
+obj.on('Weather', (msg) =>
+{
+    console.log(msg)
+})
+obj.emit('Weather', 'The Weather is having mood swings nowadays')
+
+obj.once('circus', (msg) =>
+{
+    console.log(msg)
+})
+obj.emit('circus', 'The circus is going to hv at the center of the City.')
+obj.emit('circus', 'The circus is now closed.')
+
+console.log(obj.getMaxListeners())
+obj.setMaxListeners(30)
+console.log(obj.getMaxListeners())
+
+
+var fun1 = () =>
+{
+    console.log("This is so much fun")
+}
+var fun2 = () =>
+{
+    console.log("The fun was for a short moment")
+}
+obj.addListener("marketOpen", fun1)   //callback
+obj.addListener("marketOpen", fun2)
+
+
+obj.emit('marketOpen')
+
+
---
+++ sanu.js
@@ -0,0 +1,42 @@
+// console.log("Kaise ho doston")
+// {
+//     var a = 10;
+// }
+// console.log(a);
+// {
+//     let b = 20;
+//     console.log(b);
+// }
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(100, 200));
+
+//arrow function
+
+// var add = (a,b)=>a+b
+// console.log(add(50, 70));
+
+// function greet() {
+//     return "sanu is a good boii!!!"
+// }
+// console.log(greet());
+
+// // ARROW function
+
+// var greet = () => "sanu is a good boii!!!"
+// console.log(greet());
+
+a = 10
+b = 20
+console.log("The value of a is ", a, "The value of b is ", b)
+console.log(`The value of a is ${a} and the value of b is ${b}`)
+
+var obj = {
+    name: "sanu",
+    age: 21,
+    greet:()=>"Sanu bhai kaise ho?"
+}
+console.log(obj.greet());
+
