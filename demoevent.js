ce9a2e4ed75783841e22d7bd0804856c32b96824
ei-sanu <someshranjanbiswal13678@gmail.com>
2026-01-28 16:40:59 +0530

    new code

--- demoevent.js
+++ demoevent.js
@@ -36,7 +36,6 @@ obj.addListener("marketOpen", fun1)   //callback
 obj.addListener("marketOpen", fun2)
 obj.emit('marketOpen')
 
-
 obj.removeListener("marketOpen", fun2)
 obj.emit('marketOpen')
 obj.removeAllListeners("marketOpen")
