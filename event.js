361906e6419ac0e945d765f46353e78564d1a1e1
ei-sanu <someshranjanbiswal13678@gmail.com>
2026-01-28 16:49:05 +0530

    updated back to the previous one

--- event.js
+++ event.js
@@ -2,6 +2,6 @@ const EventEmitter = require('events')       //import the module
 const emitter = new EventEmitter()   //created an object of the event emitter class
 emitter.on('greet', () =>
 {
-    console.log('yo js')
+    console.log('Yo JS')
 })
 emitter.emit('greet', 'Yoo whatsuppp!!!!')
