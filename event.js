052e7bb4245d3b3066c0eb2e7e9eb8e41e15305e
ei-sanu <someshranjanbiswal13678@gmail.com>
2026-01-28 16:55:11 +0530

    new code

--- event.js
+++ event.js
@@ -1,7 +1,16 @@
-const EventEmitter = require('events')       //import the module
-const emitter = new EventEmitter()   //created an object of the event emitter class
-emitter.on('greet', () =>
-{
-    console.log('Yo JS')
-})
-emitter.emit('greet', 'Yoo whatsuppp!!!!')
+// const EventEmitter = require('events')       //import the module
+// const emitter = new EventEmitter()   //created an object of the event emitter class
+// emitter.on('greet', () =>
+// {
+//     console.log('Yo JS')
+// })
+// emitter.emit('greet', 'Yoo whatsuppp!!!!')
+
+
+const EventEmitter = require('events')
+const emitter = new EventEmitter()
+emitter.on('start', () =>
+    {
+        console.log('The Process has started')
+    })
+emitter.emit('start')
